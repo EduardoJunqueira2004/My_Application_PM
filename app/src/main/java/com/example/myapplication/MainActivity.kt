bb7535bd7f0af9afe652f42980e1d737159fe723
eduardomj2004 <eduardomj2004@gmail.com>
2024-10-26 22:26:27 +0100

    Alterações intermédias Git Hub

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,31 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    //Do On Start On Puase On Stop On Resume On Destroy
+    override fun onStart() {
+        super.onStart()
+        //Fazer o Toast de Strings.xml
+
+// After TOast pelas Strings do XML  getString(R.string.app_name
+        Toast.makeText(this, getString(R.string.app_name), Toast.LENGTH_SHORT).show()
+
+
+    }
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "On Pause", Toast.LENGTH_SHORT).show()
+    }
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "On Stop ", Toast.LENGTH_SHORT).show()
+    }
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "On Resume", Toast.LENGTH_SHORT).show()
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "On Destroy", Toast.LENGTH_SHORT).show()
     }
 }
