e05b9750048b9402cc560a1b023af3bd9a60eec7
eduardomj2004 <eduardomj2004@gmail.com>
2024-10-27 13:30:25 +0000

    Alterações intermédias Git Hub

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -17,25 +18,32 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Log.d( "****TAG", "onCreate" )
+        Toast.makeText(this, "oncreate", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, R.string.app_name, Toast.LENGTH_LONG).show()
     }
-    //Do On Start On Puase On Stop On Resume On Destroy
-    override fun onStart() {
-        super.onStart()
-        //Fazer o Toast de Strings.xml
-
-// After TOast pelas Strings do XML  getString(R.string.app_name
-        Toast.makeText(this, getString(R.string.app_name), Toast.LENGTH_SHORT).show()
-
-
-    }
+    //Do  On Pause,  On Stop, On Start
     override fun onPause() {
         super.onPause()
         Toast.makeText(this, "On Pause", Toast.LENGTH_SHORT).show()
+        Log.d("****TAG ","onPause")
     }
     override fun onStop() {
         super.onStop()
         Toast.makeText(this, "On Stop ", Toast.LENGTH_SHORT).show()
+        Log.d("****TAG ","onStop")
     }
+
+    override fun onStart() {
+        super.onStart()
+        //Fazer o Toast de Strings.xml
+
+        // After TOast pelas Strings do XML  getString(R.string.app_name
+        Toast.makeText(this, "On Start ", Toast.LENGTH_SHORT).show()
+        Log.d("****TAG ","onStart")
+    }
+
+   /*
     override fun onResume() {
         super.onResume()
         Toast.makeText(this, "On Resume", Toast.LENGTH_SHORT).show()
@@ -43,5 +51,5 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         Toast.makeText(this, "On Destroy", Toast.LENGTH_SHORT).show()
-    }
+    }*/
 }
